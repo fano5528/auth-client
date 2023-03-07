169d32a832d7f0808adf36df15a6920e920ae686
ops@fanodesign.com <josefano@MacBook-Air-96.local>
2023-03-06 23:39:28 -0600

    estructura de token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.scss';
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import LogIn from './pages/LogIn';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <LogIn setToken={setToken}/>
+  }
+
   return (
   <Routes>
     <Route exact path="/" element={<Home/>}/>
+    <Route exact path="/dashboard" element={<Dashboard/>}/>
   </Routes>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,10 @@
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-import { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-    function trySubmit(e) {
-        e.preventDefault()
-        axios.post('https://sea-turtle-app-ifiyv.ondigitalocean.app/login',{email: email, password: password})
-        .then(res => {
-            console.log(res)
-        })
-    }
-
     return (
-        <div>
-            <h1 className="text-center mt-24 font-bold text-3xl">Log In</h1>
-            <form onSubmit={(e)=> trySubmit(e)} className="mt-12 flex w-[50vw] mx-auto border-black shadow padding-24 rounded-lg flex-col">
-                <input className="" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" className="mt-12 w-[90%] bg-red-500 rounded-md mx-auto">Log In</button>
-            </form>
+        <div className="flex flex-col items-center">
+        <h1 className="text-center mt-24 font-bold text-3xl">Home</h1>
+        <Link to="/dashboard" className="mt-12 w-[90%] bg-red-500 rounded-md mx-auto text-center">Go to Dashboard</Link>
         </div>
-      )
+    )
 }
