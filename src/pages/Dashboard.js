6d5368250dc6b1ce8f69c35a286bf71cab040432
ops@fanodesign.com <josefano@MacBook-Air-96.local>
2023-03-08 00:37:41 -0600

    resolver errores

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,11 +3,11 @@ import { useEffect, useState } from 'react'
 import jwt_decode from 'jwt-decode'
 
 export default function Dashboard() {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
 
     async function populateDashboard() {
         const token = localStorage.getItem('token')
-        const [firstName, setFirstName] = useState('')
-        const [lastName, setLastName] = useState('')
 
         const response = await fetch('https://sea-turtle-app-ifiyv.ondigitalocean.app/dashboard', {
             method: 'GET',
@@ -39,7 +39,7 @@ export default function Dashboard() {
         <div className="flex flex-col items-center">
         <h1 className="text-center mt-24 font-bold text-3xl"></h1>
         <ul>
-            <li>First Name: { firsName || "nose" }</li>
+            <li>First Name: { firstName || "nose" }</li>
             <li>Last Name: { lastName || "nose" }</li>
         </ul>
         <Link to="/" className="mt-12 w-[90%] bg-red-500 rounded-md mx-auto text-center">Go to Home</Link>
