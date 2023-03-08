f4ec61e476a9495471b3060dae6d672ef6a77d63
ops@fanodesign.com <josefano@MacBook-Air-96.local>
2023-03-07 23:41:22 -0600

    quitando jwt

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,34 +1,8 @@
 import { Link } from 'react-router-dom'
-import jwt from 'jsonwebtoken'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function Dashboard() {
-
-    const navigate = useNavigate()
-
-    async function populate() {
-        const data = await fetch('https://sea-turtle-app-ifiyv.ondigitalocean.app/dashboard', {
-            method: 'GET',
-            headers: {
-                'x-access-token': localStorage.getItem('token')
-            }
-        })
-    }
-
-    useEffect(() => {
-        const token = localStorage.getItem('token')
-        if (token) {
-            const user = jwt.verify(token)
-            if(!user) {
-                localStorage.removeItem('token')
-                navigate.replace('/login')
-            } else {
-                populate()
-            }
-        }
-    }, [])
-
 
     return (
         <div className="flex flex-col items-center">
